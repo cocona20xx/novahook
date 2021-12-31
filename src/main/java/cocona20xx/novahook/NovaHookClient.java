890bb50ff17f047a26b54db5042496abc0653274
Cocona <pexium128@gmail.com>
2021-12-31 17:09:30 -0500

    - Fixed a crash due to fail-hard behavior from OverrideSupplierRegistry when no suppliers are registered - Disabled testing mode for release - Added version and supplier count info logging at client launch

--- src/main/java/cocona20xx/novahook/NovaHookClient.java
+++ src/main/java/cocona20xx/novahook/NovaHookClient.java
@@ -3,6 +3,7 @@ package cocona20xx.novahook;
 import cocona20xx.novahook.api.OverrideSupplierRegistry;
 import cocona20xx.novahook.testing.TestOverrideSupplier;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +11,8 @@ import org.apache.logging.log4j.Logger;
 public class NovaHookClient implements ClientModInitializer {
 
 	public static final Logger LOGGER = LogManager.getLogger("novahook");
-	public static final boolean TESTS_ACTIVE = true;
+	public static final boolean TESTS_ACTIVE = false;
+	public static final String VERSION_STRING = "1.0.0";
 
 	@Override
 	public void onInitializeClient() {
@@ -18,5 +20,6 @@ public class NovaHookClient implements ClientModInitializer {
 			LOGGER.warn("NOVAHOOK TEST MODE IS ACTIVE, DISABLE BEFORE RELEASE!");
 			OverrideSupplierRegistry.register(new TestOverrideSupplier(), new Identifier("novahook", "testing_supplier"));
 		}
+		LOGGER.info("NovaHook version: " + VERSION_STRING + " initialized! Loaded " + Integer.toString(OverrideSupplierRegistry.supplierCount()) + " Override Suppliers.");
 	}
 }
